2d6e3a905fdba3782c815d82180f66d99d2c7596
IvanovAndriy <paladin.ai.303@gmail.com>
2021-06-07 23:38:48 +0300

    Add files via upload

--- ArrayInt.java
+++ ArrayInt.java
@@ -1,9 +1,16 @@
 package homework_l3;
 
+import java.util.Arrays;
 
 public class ArrayInt {
 
-	public static int indexOf(int ar[], int num) { //Complexity O[N]
+	public static int[] arr = { 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7 };
+	public static int[] arrEqualElements = { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
+	public static int[] arrSortTest = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 10, 9, 7, 6, 3, 2, 1 };
+	public static int[] arrEmpty = {};
+	public static int[] arrExample = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	
+	public static int indexOf(int ar[], int num) { 
 		int i = 0;
 		while ( i < ar.length &&  ar[i] != num ) {
 			i++;
@@ -29,74 +36,82 @@ public class ArrayInt {
 		}
 		return middle;
 	}
-	public static void sort(short arr[]) {
-	for (int i = 0; i<arr.length; i++)
-		for (int j=0; j<arr.length; j++)
-			if (arr[i] < arr[j]) {
-				short temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			} 
+	
+	static final int MAX_int = (1 << 15) - 1;
+	
+	public static boolean isSum2(int[] ar, int sum) {
+		
+		boolean helper[] = new boolean[sum == MAX_int ? sum : sum + 1];
+		int first = 0;
+		int second = 0;
+		boolean res = false;
+		for (int i = 0; i < ar.length; i++) {
+			first = ar[i];
+			if (first <= sum) {
+				second = (int) (sum - first);
+				if (helper[second]) {
+					res = true;
+					break;
+				} else {
+					helper[first] = true;
+				}
+			}
+		}
+		return res;
 	}
-	public static void displayArray(short arr[])	{
-		System.out.println("Sorted array:");
+	public static void sort(int ar[]) {
+		int counters[] = new int[MAX_int];
+		//index of counters is one number from ar
+		//value at index is count of the number from ar
+		//for example, counter[100] == 3 means 100 occurs in ar 3 times
+		fillCounters(counters, ar);
+		fillSortedArray(counters, ar);
+	}
+	private static void fillSortedArray(int[] counters, int[] ar) {
+		int arInd = 0;
+		for(int i = 0; i < counters.length && arInd < ar.length; i++) {
+			for (int j = 0; j < counters[i]; j++) {
+				ar[arInd++] = (int) i;
+			}
+		}
+		
+	}
+	private static void fillCounters(int[] counters, int[] ar) {
+		int index = 0;
+		for (int i = 0; i < ar.length; i++) {
+			index = ar[i];
+			counters[index]++;
+		}
+	}
+	public static int[] addNumber(int ar[], int num) {
+		int[] res = Arrays.copyOf(ar, ar.length + 1);
+		res[res.length - 1] = num;
+		return res;
+	}
+	public static int[] insertNumber(int ar[], int index, int num) {
+		if (index < 0  || index > ar.length) {
+			return ar;
+		}
+		int res[] = new int[ar.length + 1];
+		System.arraycopy(ar, 0, res, 0, index);
+		res[index] = num;
+		System.arraycopy(ar, index, res, index + 1, ar.length - index);
+		return res;
+	}
+	public static void displayArray(int arr[])	{
 		for (int i=0; i < arr.length; i++)
 			System.out.print("A[" + i + "] = " + arr[i] + "; ");
-		System.out.println("");
-		System.out.println("Results:");
 	}
-	public static boolean isSumTemp(short arr[], short sum)
-	{
-		boolean result = false;
-		short firstValue = -1,  secondValue = -1;
-		int firstVauleIdx = -1, secondValueIdx = -1;
-		
-		
-		for ( int i = arr.length-1 ; i >= 0; i-- )
-		{
-			    if ( (arr[i] <= sum) & (secondValue == -1))
-				{
-					firstValue = arr[i];
-					firstVauleIdx = i;
-					secondValue = (short)(sum - arr[i]);
-				}
-				
-			    if (i > 0) { 
-					if (arr[i] + arr[i-1] == sum){
-					firstValue = arr[i];
-					firstVauleIdx = i;
-					secondValue = arr[i-1];
-					secondValueIdx = i-1; 
-					System.out.println("A[" + firstVauleIdx + "] = " + firstValue +  "; A[" + secondValueIdx + "] = " + secondValue + ";");
-					System.out.println(firstValue +  " + " + secondValue + " = " + sum);
-					result = true;
-					break;
-					}
-				}
-			    
-			    if (arr[i] == secondValue)
-				{
-					secondValueIdx = i;
-					System.out.println("A[" + firstVauleIdx + "] = " + firstValue +  "; A[" + secondValueIdx + "] = " + arr[i] + ";");
-					System.out.println(firstValue +  " + " + secondValue + " = " + sum);
-					result = true;
-					break;
-				}
-			    
-				
-		}
-		return result; 
-	}
-	
-	public static boolean isSum2(short arr[], short sum) {
+	//one loop 	
+	public static boolean myIsSum2(int arr[], int sum) {
 	boolean result = false;
 	int i = 0; 
 	int k = 1;
-	short val1 = 0, val2 = 0;
+	int val1 = 0, val2 = 0;
 	while (i < arr.length)
 	{
 		val1 =  arr[i];
-		val2 = (short)(sum - val1);
+		val2 = (int)(sum - val1);
 		if ((arr[k] == val2) & ( i != k ) ) {
 			val2 = arr[k];
 			System.out.println("Input: " + sum + ". Arr[" + i + "]=" + val1 + "; Arr[" + k + "]=" + val2 + "; (" + val1 + " + " + val2 + " = " + sum + ")");
@@ -116,4 +131,119 @@ public class ArrayInt {
 	}
 	return result;	
 	}
+	public static int binaryCount(int[] arr, int num)
+	{
+		int lLimit = 0, rLimit = arr.length-1, result = -1;
+		int firstIdx = 0, lastIdx = 0;
+		boolean leftFirst =  true;
+		boolean rightFirst = false;
+		int iterationsCount = 0;
+		while (lLimit <= rLimit) {
+			iterationsCount++;
+			int midPos = ((lLimit + rLimit) / 2);
+			if (arr[midPos] == num ) {
+				result = midPos;
+				if (leftFirst)
+				rLimit = midPos - 1;
+				if (rightFirst)
+				lLimit = midPos + 1;
+			}
+			else {
+				if (num < arr[midPos]) {
+					rLimit = midPos - 1;
+				}
+				else {
+					lLimit = midPos + 1;
+				}
+			}
+			
+			if (rightFirst) {
+				if (lLimit > rLimit)  {
+					if ( (firstIdx >= 0) & (lastIdx >= 0) ) {
+					//System.out.println("Iterations count: " + iterationsCount);
+					lastIdx = result;
+					//System.out.println("Interval of " + num + " is from " + firstIdx + " to " + lastIdx);
+					result = lastIdx - firstIdx + 1;
+					}
+					break;
+				}
+			}
+		if (lLimit > rLimit) {
+			leftFirst = false;
+			lLimit = 0; 
+			rLimit = arr.length-1;
+			firstIdx = result;
+			rightFirst = true;
+		}
+		}
+		return result;  
+	}
+	
+	
+	public static int binarySearch(int[] arr, int num) {
+		int lLimit = 0, rLimit = arr.length-1, result = -1;
+		if (arr.length == 0) {
+			return 0;
+		}
+		if (arr[0] > num) {
+			return 0;
+		}
+		if (arr[arr.length-1] < num) {
+			return arr.length;
+		}
+		while (lLimit <= rLimit) {
+			int midPos = ((lLimit + rLimit) / 2);
+			if (arr[midPos] == num ) {
+				result = midPos;
+				rLimit = midPos - 1;
+			}
+			else {
+				if (num < arr[midPos]) {
+					rLimit = midPos - 1;
+				}
+				else {
+					lLimit = midPos + 1;
+				}
+				if ((arr[midPos-1] < num) & (num < arr[midPos+1]))
+					result = midPos;
+			}
+		}	
+		return result;  	
+	}
+	
+	
+	
+	public static int[] addNumberSorted(int ar[], int num)
+	{
+		int arr[] = new int[ar.length+1];
+		int pos = binarySearch(ar, num);
+		System.arraycopy(ar, 0, arr, 0, pos);
+		arr[pos] = num;
+		System.arraycopy(ar, pos, arr, pos+1, ar.length-pos);
+		return arr;
+		
+	}
+	
+	public static int[] removeNumber(int ar[], int index)
+	{
+		int arr[] = new int[ar.length-1];
+		if (index == 0) { 
+			index = 1; 
+		}
+		if (index > arr.length) {
+			index = (int)arr.length;
+		}
+			
+		
+		System.arraycopy(ar, 0, arr, 0, index-1);
+		System.arraycopy(ar, index, arr, index-1, ar.length-index);
+		return arr;
+		
+		
+	}
+	//написать тесты для 3 методов и binarysearch
+	
+	
+	
+	
 }
--- ArrayIntTests.java
+++ ArrayIntTests.java
@@ -2,24 +2,80 @@ package homework_l3;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 import homework_l3.ArrayInt;
 //import sun.nio.fs.ExtendedOptions.InternalOption;
 
 class ArrayIntTests {
 
+	private static final int N_NUMBERS = 1_000_000;
+
 	@Test
 	void testMyArray() {
-		ArrayInt.sort(BitOperations.arr);
-		short[] test = { 0, 1, 2, 7, 8, 8, 18, 20, 27, 30, 60, 77, 81, 89, 99, 100 }; 
-		assertArrayEquals(test, BitOperations.arr);
-		ArrayInt.displayArray(BitOperations.arr);
-		assertEquals(true, ArrayInt.isSum2(BitOperations.arr, (short)( 16 )));
+		/*assertEquals(true, ArrayInt.isSum2(BitOperations.arr, (short)( 16 )));
 		assertEquals(true, ArrayInt.isSum2(BitOperations.arr, (short)( 77 )));
 		assertEquals(true, ArrayInt.isSum2(BitOperations.arr, (short)( 100 )));
 		assertEquals(true, ArrayInt.isSum2(BitOperations.arr, (short)( 199 )));
 		assertEquals(true, ArrayInt.isSum2(BitOperations.arr, (short)( 170 )));
 		assertEquals(true, ArrayInt.isSum2(BitOperations.arr, (short)( 85 )));
+		assertEquals(true, ArrayInt.isSum2(BitOperations.arr, (short)( 88 )));*/
+		//homework lesson 6
+		//sort test
+		int[] sortTest = new int[ArrayInt.arrSortTest.length];
+		System.arraycopy(ArrayInt.arrSortTest, 0, sortTest, 0, ArrayInt.arrSortTest.length);
+		Arrays.sort(sortTest);
+		int[] myArray = new int[ArrayInt.arrSortTest.length];
+		System.arraycopy(ArrayInt.arrSortTest, 0, myArray, 0, ArrayInt.arrSortTest.length);
+		ArrayInt.sort(myArray);
+		assertArrayEquals(sortTest, myArray);
+		//add number sorted
+		int[] arrTestEmpty = new int[1];
+		arrTestEmpty[0] = 10;
+		assertArrayEquals(arrTestEmpty, ArrayInt.addNumberSorted(ArrayInt.arrEmpty, 10));
+		int[] arrExampleInsert = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int[] arrResultInsert = {0, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9 };
+		assertArrayEquals(arrResultInsert, ArrayInt.addNumberSorted(arrExampleInsert, 5));
+		//remove number sorted
+		int[] arrExampleRemove = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int[] arrResultRemove = {0, 1, 2, 3, 4, 6, 7, 8, 9 };
+		assertArrayEquals(arrResultRemove, ArrayInt.removeNumber(arrExampleRemove, 6));
+		//test binarycount
+		assertEquals(1, ArrayInt.binaryCount(ArrayInt.arr, 1));// 1 element 1 in arr
+		assertEquals(2, ArrayInt.binaryCount(ArrayInt.arr, 2));// 2 elements 2 in arr
+		assertEquals(3, ArrayInt.binaryCount(ArrayInt.arr, 3));// 3 elements 3 in arr
+		assertEquals(4, ArrayInt.binaryCount(ArrayInt.arr, 4));// 4 elements 4 in arr
+		assertEquals(5, ArrayInt.binaryCount(ArrayInt.arr, 5));// 5 elements 5 in arr
+		assertEquals(6, ArrayInt.binaryCount(ArrayInt.arr, 6));// 6 elements 6 in arr
+		assertEquals(7, ArrayInt.binaryCount(ArrayInt.arr, 7));// 7 elements 7 in arr
+		assertEquals(10, ArrayInt.binaryCount(ArrayInt.arrEqualElements, 5)); // without ( if a[0] == num and a[n] == num) 
+		assertEquals(-1, ArrayInt.binaryCount(ArrayInt.arrEqualElements, 100));//no element in array or array is empty 
+		assertEquals(-1, ArrayInt.binaryCount(ArrayInt.arrEmpty, 0));//no element in array or array is empty
  	}
+	
+		
+		
+	
+	
+	@Test
+	void sortTest() {
+		short ar[] = getShortArray(N_NUMBERS);
+		//ArrayInt.sort(ar);
+		java.util.Arrays.sort(ar);
+		int lim = N_NUMBERS - 1;
+		for (int i = 0; i < lim; i++) {
+			assertTrue(ar[i] <= ar[i+1]);
+		}
+		
+	}
+	
+	private short[] getShortArray(int nNumbers)	{
+		short res[] = new short[nNumbers];
+		for (int i = 0; i < nNumbers; i++ ) {
+			res[i] = (short) (Math.random() * Short.MAX_VALUE);
+	 	}
+		return res;
+	}
 
 }
